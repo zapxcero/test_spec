4889242e5e4bcca0c8e919c5af59e4659043213d
Adrian Rodriguez <zapxcero@gmail.com>
2022-09-19 13:59:29 +0800

    Added other methods

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,5 +1,19 @@
+# frozen_string_literal: true
+
 class Calculator
-  def add(a,b)
-    a+b
+  def add(*args)
+    args.sum
+  end
+
+  def subtract(a, b)
+    a - b
+  end
+
+  def multiply(a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    a / b
   end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,9 +1,34 @@
-require './lib/calculator.rb'
+# frozen_string_literal: true
+
+require './lib/calculator'
 
 describe Calculator do
-  describe "returns the sum of two numbers" do
-    it "#add" do
-      expect(subject.add(5,2)).to eql(7)
+  describe '#add' do
+    it 'returns the sum of two numbers' do
+      expect(subject.add(5, 2)).to eql(7)
+    end
+
+    it 'returns the sum of more than two numbers' do
+      calculator = Calculator.new
+      expect(calculator.add(2, 5, 7)).to eql(14)
+    end
+  end
+
+  describe "#subtract" do
+    it "returns the difference of two numbers" do
+      expect(subject.subtract(5,3)).to eql(2)
+    end
+  end
+
+  describe "#multiply" do
+    it "returns the product of two numbers" do
+      expect(subject.multiply(5,3)). to eql(15)
+    end
+  end
+
+  describe "#divide" do
+    it "returns the quotient of two numbers" do
+      expect(subject.divide(15,3)).to eql(5)
     end
   end
 end
