6ec0dea98ac3c030827ce141670159db656d04f5
Adrian Rodriguez <zapxcero@gmail.com>
2022-09-19 14:02:25 +0800

    Run with rubocop

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -14,21 +14,21 @@ describe Calculator do
     end
   end
 
-  describe "#subtract" do
-    it "returns the difference of two numbers" do
-      expect(subject.subtract(5,3)).to eql(2)
+  describe '#subtract' do
+    it 'returns the difference of two numbers' do
+      expect(subject.subtract(5, 3)).to eql(2)
     end
   end
 
-  describe "#multiply" do
-    it "returns the product of two numbers" do
-      expect(subject.multiply(5,3)). to eql(15)
+  describe '#multiply' do
+    it 'returns the product of two numbers' do
+      expect(subject.multiply(5, 3)).to eql(15)
     end
   end
 
-  describe "#divide" do
-    it "returns the quotient of two numbers" do
-      expect(subject.divide(15,3)).to eql(5)
+  describe '#divide' do
+    it 'returns the quotient of two numbers' do
+      expect(subject.divide(15, 3)).to eql(5)
     end
   end
 end
